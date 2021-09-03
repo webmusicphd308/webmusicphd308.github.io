ba77e8efcaade0bbc0c36731804fbc279b79814e
phamhuynhduc88 <64743390+phamhuynhduc88@users.noreply.github.com>
2021-09-03 14:38:51 +0700

    update

---
+++ js/music-list.js
@@ -0,0 +1,177 @@
+let allMusic = [
+    {
+      name: "3103-7",
+      artist: "W/n x ( Nâu,Duongg,Titie )",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Chúng Ta Sau Này",
+      artist: "T.R.I",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Sài Gòn Đau Lòng Quá",
+      artist: "Hứa Kim Tuyền, Hoàng Duyên ",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Thê Lương",
+      artist: "Phúc Chinh",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Hoa Nở Không Màu",
+      artist: "Hoài Lâm",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Beautiful In White",
+      artist: "Shane Filan",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Dạ Vũ",
+      artist: "Tăng Duy Tân",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+      name: "Một Bước Yêu Vạn Dặm Đau",
+      artist: "Mr.Siro",
+      img: "music-8",
+      src: "music-8"
+    },
+    {
+      name: "Rồi Tới Luôn",
+      artist: "Nal",
+      img: "music-9",
+      src: "music-9"
+    },
+    {
+      name: "Why Not Me",
+      artist: "Enrique Iglesias",
+      img: "music-10",
+      src: "music-10"
+    },
+    {
+      name: "Sao Em Lại Tắt Máy",
+      artist: "Phạm Nguyên Ngọc ft.Vanh",
+      img: "music-11",
+      src: "music-11"
+    },
+    {
+      name: "NumB",
+      artist: "Linkin Park",
+      img: "music-12",
+      src: "music-12"
+    },
+    {
+      name: "Thuận Theo Ý Trời",
+      artist: "Bùi Anh Tuấn",
+      img: "music-13",
+      src: "music-13"
+    },
+    {
+      name: "The Night",
+      artist: "AVICII",
+      img: "music-14",
+      src: "music-14"
+    },
+    {
+      name: "Dancing With Your Ghost",
+      artist: "Sasha Alex Sloan ",
+      img: "music-15",
+      src: "music-15"
+    },
+    {
+      name: "Đi Về Nhà",
+      artist: "Đen x JustaTee",
+      img: "music-16",
+      src: "music-16"
+    },
+    {
+      name: "Tháng Mấy Em Nhớ Anh",
+      artist: "Hà Anh Tuấn",
+      img: "music-17",
+      src: "music-17"
+    },
+    {
+      name: "Cưới Thôi",
+      artist: "Masew",
+      img: "music-18",
+      src: "music-18"
+    },
+    {
+      name: "Kẹo Bông Gòn",
+      artist: "H2K x Trunky",
+      img: "music-19",
+      src: "music-19"
+    },
+    {
+      name: "I Know",
+      artist: "EunJung ",
+      img: "music-20",
+      src: "music-20"
+    },
+    {
+      name: "Cô Đơn Trong Nhà Mình",
+      artist: "Hoài Lâm",
+      img: "music-21",
+      src: "music-21"
+    },
+    {
+      name: "Hạnh Phúc Mới",
+      artist: "MTP x KICM",
+      img: "music-22",
+      src: "music-22"
+    },
+    {
+      name: "Không Muốn Yêu Lại Càng Say Đắm",
+      artist: "Mr.Siro",
+      img: "music-23",
+      src: "music-23"
+    },
+    {
+      name: "Chia Cách Bình Yên",
+      artist: "Quốc Thiên",
+      img: "music-24",
+      src: "music-24"
+    },
+    {
+      name: "Lỗi Tại Anh",
+      artist: "Alex Lâm",
+      img: "music-25",
+      src: "music-25"
+    },
+    {
+      name: "不该用情",
+      artist: "Mạc Khiếu",
+      img: "music-26",
+      src: "music-26"
+    },
+    {
+      name: "Khi Cô Đơn Em Nhớ Ai",
+      artist: "JUUN D FT. R.TEE ",
+      img: "music-27",
+      src: "music-27"
+    },
+    {
+      name: "Ngàn Lần Khắc Tên Em",
+      artist: "Hoài Lâm",
+      img: "music-28",
+      src: "music-28"
+    },
+    {
+      name: "世界這麼大還是遇見你",
+      artist: "程響",
+      img: "music-29",
+      src: "music-29"
+    },
+    
+  ];
